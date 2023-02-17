d1d713c406c630821e65341f24b9248a6f6b56c5
Matthew Rosenberg <rosenmatt1@gmail.com>
2023-02-16 19:46:02 -0700

    Added audio sound variable for button

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import './App.css';
+import data from "./data.json";
+import image from "./chuck-norris.png"
 
 function App() {
+  console.log(data)
+
+  const sound = new Audio("upper-cut.mp3")
+
+
   return (
     <div className="App">
-      Check Norris App
+      <h1>Hello Chuck</h1>
+
+      <img className="chuck"  alt="chuck" src={image}/>
+
+      <button className="button">
+        {/* <i className="fa-solid fa-hand-fist fa-2xl" style={{ fontSize: "200px" }} ></i> */}
+        <i className="fas fa-fist-raised" style={{ fontSize: "50px" }}></i>
+      </button>
+
     </div>
   );
 }
