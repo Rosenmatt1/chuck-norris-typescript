1f149ec9363c7748b9a972ca868ea3245134f72a
Matthew Rosenberg <rosenmatt1@gmail.com>
2023-02-17 21:40:26 -0700

    Added conditional logic to display quote

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@ import image from "./chuck-norris.png"
 function App() {
   const [quotes] = useState(data.jokes);
   const [quote, setQuote] = useState("")
-  // console.log(data)
 
   const sound = new Audio("upper-cut.mp3")
 
@@ -14,8 +13,7 @@ function App() {
     console.log(quotes)
     const randomNumber = Math.floor(Math.random() * quotes.length);
     setQuote(quotes[randomNumber].joke)
-    // quotes[randomNumber].joke
-    console.log(quotes[randomNumber].joke)
+    playSound()
   }
 
   const playSound = () => {
@@ -25,19 +23,19 @@ function App() {
   return (
     <div className="App">
 
-      <h1>Hello Chuck</h1>
+      <h1> Hello Chuck! </h1>
 
       <img className="chuck" alt="chuck" src={image} />
 
       <div> An app for randomly generating chuck norris quotes. </div>
 
-      <div className="quoteBox">
+      {(quote.length > 0) && <div className="quoteBox">
         <div className="quote"> {quote} </div>
-      </div>
+      </div>}
 
       <button className="button" onClick={randomQuote}>
         <div className="karate"> Karate Chop! </div>
-        <i className="fas fa-fist-raised" style={{ fontSize: "3rem", color: "white" }}></i>
+        <i className="fas fa-fist-raised fist" style={{ fontSize: "3rem", color: "white" }}></i>
       </button>
 
     </div>
